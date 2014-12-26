dc62a6cc17dd6c54c9a445f088267500e84da81a
Azzamok <azzamok@hotmail.com>
2014-12-27 01:51:58 +1100

    Adding existing code.

---
+++ SimpleGUI/SimpleGUI.py
@@ -0,0 +1,81 @@
+import sys
+from tkinter import *
+
+root = Tk()
+herp = StringVar()
+
+
+def mhello():
+    xtext = herp.get() #Gets the text user typed in box
+    xlabel = Label(root, text=xtext) #Makes a label using the text
+    xlabel.grid(row=0, column=4) #Place label
+    return
+    #xlabel = Label(text='hello world')
+    #xlabel.grid(row=0, column=4)
+
+def mNew():
+    mlabel3 = Label(root, text='You clicked New')
+    mlabel3.place(x=200, y=200)
+    return
+
+#Window
+root.title("Simple GUI")
+#Size
+root.geometry("640x480")
+
+mlabel = Label(text='blah', fg='red', bg='blue')
+#mlabel.pack() #Places label at the top middle
+#mlabel.place(x=150, y=150) #Places label at custom co-ords
+
+#.grid() Think excel spreadsheets
+mlabel.grid(row=0, column=0, sticky=W) #Align left
+
+mlabel2 = Label(text='hello there', fg='green', bg='yellow')
+mlabel2.grid(row=1, column=0)
+
+mlabel3 = Label(text='potatoes', fg='purple',bg='cyan')
+mlabel3.grid(row=0, column=1)
+
+mbutton = Button(text='OK', command = mhello)
+#mbutton.place(x=300, y=300)
+mbutton.grid(row=0, column=2)
+
+#Entry Box
+mEntry = Entry(root, textvariable=herp)
+mEntry.grid(row=0, column=3)
+#mEntry.place(x=400, y=450)
+
+#Menu Bar
+menubar = Menu(root)
+
+#File menu
+filemenu = Menu(menubar, tearoff = 0)
+filemenu.add_command(label = 'New', command = mNew)
+filemenu.add_command(label = 'Open')
+filemenu.add_command(label = 'Save As...')
+filemenu.add_command(label = 'Close')
+menubar.add_cascade(label = 'File', menu = filemenu)
+
+#Help menu
+helpmenu = Menu(menubar, tearoff = 0)
+helpmenu.add_command(label = 'Help Docs')
+helpmenu.add_command(label = 'About')
+menubar.add_cascade(label = 'Help', menu = helpmenu)
+
+
+#Scrollbar
+#scrollbar = Scrollbar(root)
+#scrollbar.pack(side = RIGHT, fill = Y)
+#myList = Listbox(root, yscrollcommand = scrollbar.set)
+#myList.pack(side = LEFT, fill = BOTH)
+#scrollbar.config(command = myList.yview)
+
+root.config(menu = menubar)
+
+
+
+
+
+
+#For windows
+root.mainloop()
