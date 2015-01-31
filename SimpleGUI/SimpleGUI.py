84dfed881017cb72b496c4a28e984073e9e537ae
Azzamok <azzamok@hotmail.com>
2015-02-01 01:35:49 +1100

    Made documentation nicer to read.

--- SimpleGUI/SimpleGUI.py
+++ SimpleGUI/SimpleGUI.py
@@ -22,24 +22,28 @@ def mQuit():
     return
 
 
-#Title at the top
+"""Title at the top"""
 root.title("Simple GUI")
 
-#Window size
+
+"""Window size"""
 root.geometry("720x480")
 
-#Creating a label
-#text = what you want it to display
-#fg = foreground colour (hex)
-#bg = background colour (hex)
-#font = (Font type, size)
-mlabel = Label(text='blah, this is text', fg='#3aec61', bg='#07c7dd', font=("Calibri", 18))
 
+"""
+Creating a label
+
+text = what you want it to display
+fg = foreground colour (hex)
+bg = background colour (hex)
+font = (Font type, size)
+"""
+mlabel = Label(text='blah, this is text', fg='#3aec61', bg='#07c7dd', font=("Calibri", 18))
 #mlabel.pack() #Places label at the top middle
 #mlabel.place(x=150, y=150) #Places label at custom co-ords
 
 
-#.grid() Think excel spreadsheets
+""".grid() command Think excel spreadsheets"""
 mlabel.grid(row=0, column=0, sticky=W) #Align left
 
 mlabel2 = Label(text='hello there', fg='green', bg='yellow')
@@ -48,16 +52,20 @@ mlabel2.grid(row=1, column=0)
 mlabel3 = Label(text='potatoes', fg='purple',bg='cyan')
 mlabel3.grid(row=0, column=1)
 
+
+"""Button"""
 mbutton = Button(text='OK', command = mHello)
 #mbutton.place(x=300, y=300)
 mbutton.grid(row=0, column=2)
 
-#Entry Box
+
+"""Entry Box"""
 mEntry = Entry(root, textvariable=herp)
 mEntry.grid(row=0, column=3)
 #mEntry.place(x=400, y=450)
 
-#Menu Bar
+
+"""Menu Bar"""
 menubar = Menu(root)
 
 #File menu
@@ -75,16 +83,16 @@ helpmenu.add_command(label = 'About')
 menubar.add_cascade(label = 'Help', menu = helpmenu)
 
 
+root.config(menu = menubar)
+
+
+"""Doesn't work..."""
 #Scrollbar
 #scrollbar = Scrollbar(root)
 #scrollbar.pack(side = RIGHT, fill = Y)
 #myList = Listbox(root, yscrollcommand = scrollbar.set)
 #myList.pack(side = LEFT, fill = BOTH)
 #scrollbar.config(command = myList.yview)
-
-root.config(menu = menubar)
-
-
 
 
 
